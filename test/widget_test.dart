bc558d81850fae1ad48ea5e9245acdc6cbe64fcc
Mathis <matblairon@gmail.com>
2025-03-24 14:45:43 +0100

    fix(CI): fix CI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,10 +10,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pokedeal/main.dart';
 
 void main() {
-  test('failedTest', () {
-    expect(1, 2);
-  });
-
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
