ef7d426712ab0300109dca3fa21a7b97940295be
Mathis <matblairon@gmail.com>
2025-03-31 12:30:15 +0200

    style: TextField style dans le theme

--- lib/theme/custom_theme.dart
+++ lib/theme/custom_theme.dart
@@ -8,14 +8,46 @@ ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
   ),
   scaffoldBackgroundColor: const Color(0xFFF5F5F5),
+  inputDecorationTheme: InputDecorationTheme(
+    filled: true,
+    fillColor: Colors.white,
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8.0),
+      borderSide: BorderSide(color: Colors.grey),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8.0),
+      borderSide: BorderSide(color: Colors.grey),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8.0),
+      borderSide: BorderSide(color: Color(0xFFEE6B2F)),
+    ),
+  ),
 );
 
 ThemeData darkTheme = ThemeData(
-  colorScheme: ColorScheme.light(
+  colorScheme: ColorScheme.dark(
     primary: const Color(0xFFEE6B2F),
     secondary: const Color(0xFF30A7D7),
     tertiary: const Color(0xFFD9D9D9),
     brightness: Brightness.dark,
   ),
-  scaffoldBackgroundColor: const Color(0xFFF5F5F5),
+  scaffoldBackgroundColor: const Color(0xFF121212),
+  inputDecorationTheme: InputDecorationTheme(
+    filled: true,
+    fillColor: Colors.grey[800],
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8.0),
+      borderSide: BorderSide(color: Colors.grey),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8.0),
+      borderSide: BorderSide(color: Colors.grey),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8.0),
+      borderSide: BorderSide(color: Color(0xFFEE6B2F)),
+    ),
+  ),
 );
