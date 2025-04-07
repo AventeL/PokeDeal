a90a5f78be55b8cc4e41266889e755e270941b11
Mathis <matblairon@gmail.com>
2025-04-07 14:21:06 +0200

    style: header sans safearea

--- lib/features/authentication/presentation/pages/authentication_page.dart
+++ lib/features/authentication/presentation/pages/authentication_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:pokedeal/core/widgets/empty_space.dart';
 import 'package:pokedeal/features/authentication/presentation/bloc/authentication_bloc.dart';
 import 'package:pokedeal/features/authentication/presentation/pages/register_page_view.dart';
 
@@ -54,33 +55,36 @@ class _AuthenticationPageState extends State<AuthenticationPage>
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: 250,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.vertical(
-                        bottom: Radius.circular(20),
-                      ),
-                      gradient: LinearGradient(
-                        colors: [
-                          Theme.of(context).colorScheme.secondary,
-                          Theme.of(context).colorScheme.primary,
-                        ],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                      ),
+    return Column(
+      children: [
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.vertical(
+                      bottom: Radius.circular(20),
+                    ),
+                    gradient: LinearGradient(
+                      colors: [
+                        Theme.of(context).colorScheme.secondary,
+                        Theme.of(context).colorScheme.primary,
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).padding.top,
+                      bottom: 40,
                     ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        const SizedBox(height: 20),
+                        20.height,
                         Text(
                           "PokeDeal",
                           style: Theme.of(context).textTheme.headlineLarge
@@ -96,129 +100,131 @@ class _AuthenticationPageState extends State<AuthenticationPage>
                       ],
                     ),
                   ),
-                  Container(
-                    margin: EdgeInsets.all(20),
-                    child:
-                        BlocConsumer<AuthenticationBloc, AuthenticationState>(
-                          listener: (context, state) {
-                            if (state is AuthenticationError && mounted) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(content: Text(state.message)),
-                              );
-                            }
-                          },
-                          builder: (context, state) {
-                            if (state is AuthenticationLoading) {
-                              return const Center(
-                                child: CircularProgressIndicator(),
-                              );
-                            }
+                ),
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: BlocConsumer<AuthenticationBloc, AuthenticationState>(
+                    listener: (context, state) {
+                      if (state is AuthenticationError && mounted) {
+                        ScaffoldMessenger.of(
+                          context,
+                        ).showSnackBar(SnackBar(content: Text(state.message)));
+                      }
+                    },
+                    builder: (context, state) {
+                      if (state is AuthenticationLoading) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
 
-                            return Column(
+                      return Column(
+                        children: [
+                          TabBar(
+                            controller: _tabController,
+                            indicatorSize: TabBarIndicatorSize.tab,
+                            tabs: const [
+                              Tab(text: 'Connexion'),
+                              Tab(text: 'Inscription'),
+                            ],
+                            indicatorPadding: const EdgeInsets.symmetric(
+                              horizontal: 20,
+                            ),
+                            dividerColor: Colors.transparent,
+                          ),
+                          SizedBox(
+                            height: 500,
+                            child: TabBarView(
+                              controller: _tabController,
                               children: [
-                                TabBar(
-                                  controller: _tabController,
-                                  indicatorSize: TabBarIndicatorSize.tab,
-                                  tabs: const [
-                                    Tab(text: 'Connexion'),
-                                    Tab(text: 'Inscription'),
-                                  ],
-                                  indicatorPadding: const EdgeInsets.symmetric(
-                                    horizontal: 20,
-                                  ),
-                                  dividerColor: Colors.transparent,
+                                LoginPageView(
+                                  emailController: emailController,
+                                  passwordController: passwordController,
                                 ),
-                                SizedBox(
-                                  height: 500,
-                                  child: TabBarView(
-                                    controller: _tabController,
-                                    children: [
-                                      LoginPageView(
-                                        emailController: emailController,
-                                        passwordController: passwordController,
-                                      ),
-                                      RegisterPageView(
-                                        emailController: emailController,
-                                        passwordController: passwordController,
-                                        confirmPasswordController:
-                                            confirmPasswordController,
-                                      ),
-                                    ],
-                                  ),
+                                RegisterPageView(
+                                  emailController: emailController,
+                                  passwordController: passwordController,
+                                  confirmPasswordController:
+                                      confirmPasswordController,
                                 ),
                               ],
-                            );
-                          },
-                        ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ValueListenableBuilder<int>(
-              valueListenable: _tabIndexNotifier,
-              builder: (context, index, _) {
-                return ElevatedButton(
-                  onPressed: () {
-                    String email = emailController.text;
-                    String password = passwordController.text;
-                    if (index == 0) {
-                      if (email.isEmpty || password.isEmpty) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('Veuillez remplir tous les champs'),
-                          ),
-                        );
-                        return;
-                      }
-                      _onLogin(email, password);
-                    } else {
-                      String confirmPassword = confirmPasswordController.text;
-                      if (email.isEmpty ||
-                          password.isEmpty ||
-                          confirmPassword.isEmpty) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('Veuillez remplir tous les champs'),
-                          ),
-                        );
-                        return;
-                      }
-                      if (password != confirmPassword) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text(
-                              'Les mots de passe ne correspondent pas',
                             ),
                           ),
-                        );
-                        return;
-                      }
-                      context.go(
-                        '/get_info_profile',
-                        extra: {'email': email, 'password': password},
+                        ],
                       );
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    minimumSize: const Size(double.infinity, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
+                    },
                   ),
-                  child: Text(
-                    index == 0 ? "Se connecter" : "S'inscrire",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                );
-              },
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(
+            left: 16,
+            right: 16,
+            bottom: MediaQuery.of(context).padding.bottom,
+            top: 16,
+          ),
+          child: ValueListenableBuilder<int>(
+            valueListenable: _tabIndexNotifier,
+            builder: (context, index, _) {
+              return ElevatedButton(
+                onPressed: () {
+                  String email = emailController.text;
+                  String password = passwordController.text;
+                  if (index == 0) {
+                    if (email.isEmpty || password.isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Veuillez remplir tous les champs'),
+                        ),
+                      );
+                      return;
+                    }
+                    _onLogin(email, password);
+                  } else {
+                    String confirmPassword = confirmPasswordController.text;
+                    if (email.isEmpty ||
+                        password.isEmpty ||
+                        confirmPassword.isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Veuillez remplir tous les champs'),
+                        ),
+                      );
+                      return;
+                    }
+                    if (password != confirmPassword) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text(
+                            'Les mots de passe ne correspondent pas',
+                          ),
+                        ),
+                      );
+                      return;
+                    }
+                    context.go(
+                      '/get_info_profile',
+                      extra: {'email': email, 'password': password},
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: Text(
+                  index == 0 ? "Se connecter" : "S'inscrire",
+                  style: TextStyle(color: Colors.white),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
